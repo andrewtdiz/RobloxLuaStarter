ab0851ef595b947620f4b0642708488f131d676d
andrewtdiz <59515127+andrewtdiz@users.noreply.github.com>
2025-03-19 15:50:45 -0500

    starter

---
+++ debug/Switchy/README.md
@@ -0,0 +1,54 @@
+# Switchy
+**Switchy** is the fastest way to work in **Roblox Studio** and **Any Code Editor**.
+
+Switchy emulates native Roblox Studio functionality of Play/Stop keybindings with any code editor, accelerating your development workflow.
+
+## Key Features
+
+- **Run Your Game Instantly**  
+  Press ANY key to run your game and switch tabs to Roblox Studio.
+  
+- **Return to Scripting with Ease**  
+  Press ANY key to stop the game and immediately switch back to your Code Editor for scripting.
+
+
+> [!CAUTION]
+> Switchy is only available on Windows operating systems at this time.
+> Switchy is currently in the early stages of development!
+
+
+## Why Use Switchy?
+
+- **Boost Productivity**: No more constant alt-tabbing between windows.
+- **Stay Focused**: Seamless switching keeps you in flow.
+- **Native Experience**: Win32 keybindings make Switchy *blazingly fast*.
+
+
+## Installation
+
+- Clone the repository:
+   ```bash
+   git clone https://github.com/spomge/switchy.git
+   ```
+
+- Install Zig:
+    if you don't have zig installed, or you don't have 0.13.0 installed, you can install it [here](https://ziglang.org/download/)!
+
+- Build the project: 
+    To build the project all you need to do is run `zig build` from the root of the project!
+
+- Usage Syntax
+  ```
+   zig build run -- "[Roblox Studio Window Title]" "[Visual Studio Code / Cursor]" "[Key to siwtch to Roblox Studio]" "[Key to switch to Visual Studio Code / Cursor]"
+   ```
+
+- Example Usage
+   ```
+   zig build run -- "MyGame - Roblox Studio" "Visual Studio Code" "F5" "F4"
+   ```
+   This listens for F5 and switchs to discord and on F4 it switches to Google
+    
+
+## License
+
+This project is licensed under the MIT License.
---
+++ debug/Switchy/src/main.zig
@@ -0,0 +1,79 @@
+const std = @import("std");
+const windows = std.os.windows;
+
+// Import custom modules
+const input = @import("input.zig");
+const window = @import("window.zig");
+const args = @import("args.zig");
+
+pub fn main() !void {
+    // Get command line arguments
+    const commandLineArgs = args.getProcessArgs() orelse return error.FailedToGetArgs;
+
+    // Check if the windows are open
+    _ = window.findAWindow(commandLineArgs[1]) orelse return error.WindowNotFound;
+    _ = window.findAWindow(commandLineArgs[2]) orelse return error.WindowNotFound;
+
+    // Check if the arguments are valid
+    if (try args.checkArgs(commandLineArgs)) {
+        // Start listening for key inputs
+        try input.listen_for_key(struct {
+            fn callback(key: []const u8) void {
+                // Get fresh command line arguments
+                const commandLineArgs2 = args.getProcessArgs() orelse return;
+
+                // Extract window titles and key bindings from arguments
+                const WindowTitle1 = commandLineArgs2[1];
+                const WindowTitle2 = commandLineArgs2[2];
+                const Key1 = commandLineArgs2[3];
+                const Key2 = commandLineArgs2[4];
+
+                // Handle key presses
+                if (std.mem.eql(u8, key, Key1)) {
+                    // Find and focus the first window
+                    // IMPORTANT: This is necessary to make the window appear on top of other windows
+                    // Without it, the window will not be focused
+                    // You can change this to whatever you want (key press or mouse click)
+                    input.send_mouse_click(std.heap.page_allocator, 1920 / 2, 1080 / 2) catch {};
+
+                    const FirstWindow = window.findAWindow(WindowTitle1) orelse {
+                        std.debug.print("Window 1 is no longer open\n", .{});
+                        return;
+                    };
+                    _ = window.showWindow(FirstWindow);
+                    _ = window.setForegroundWindow(FirstWindow);
+
+                    // If you want to do run a command after the window is focused, you can do it here
+
+                    // Example:
+                    // This hits the enter key after the window is focused
+                    // input.send_key_press(std.heap.page_allocator, &.{input.Keys.ENTER}) catch {};
+
+                    // For Roblox studio the default play key is F5
+                    input.send_key_press(std.heap.page_allocator, &.{input.Keys.F5}) catch {};
+                } else if (std.mem.eql(u8, key, Key2)) {
+
+                    // For Roblox studio the default stop key is F5 + shift
+                    input.send_key_press(std.heap.page_allocator, &.{
+                        input.Keys.F3,
+                    }) catch {};
+
+                    // Find and focus the second window
+                    const SecondWindow = window.findAWindow(WindowTitle2) orelse {
+                        std.debug.print("Window 2 is no longer open\n", .{});
+                        return;
+                    };
+                    _ = window.showWindow(SecondWindow);
+                    _ = window.setForegroundWindow(SecondWindow);
+
+                    // If you want to do run a command after the window is focused, you can do it here
+
+                    // Example:
+                    // This hits the enter key after the window is focused
+                    // input.send_key_press(std.heap.page_allocator, &.{input.Keys.ENTER}) catch {};
+
+                }
+            }
+        }.callback);
+    }
+}
